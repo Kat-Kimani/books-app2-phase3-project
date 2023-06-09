99334a418f4fc5c233b823b0c69c906cb4871c8b
catherine <wachukimani@gmail.com>
2023-06-09 11:08:30 +0300

    seeded data with faker

--- models.py
+++ models.py
@@ -2,19 +2,11 @@
 
 import os
 import sys
-from sqlalchemy.orm import relationship, backref
+
 
 sys.path.append(os.getcwd)
-from sqlalchemy import (
-    create_engine,
-    ForeignKey,
-    Column,
-    Integer,
-    String,
-    MetaData,
-    Table,
-)
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import create_engine, ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine("sqlite:///books2.db", echo=True)
@@ -32,6 +24,7 @@ class Book(Base):
 
     author = relationship("Author", back_populates="books")
     genre = relationship("Genre", back_populates="books")
+    reviews = relationship("Review", back_populates="book")  # Added relationship
 
     def __repr__(self):
         return f"Book(id={self.id}, title='{self.title}')"
@@ -60,3 +53,34 @@ class Genre(Base):
 
 
 ###########################################################
+
+
+###########################################################
+class Borrower(Base):
+    __tablename__ = "borrowers"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(), nullable=False)
+    last_name = Column(String(), nullable=False)
+
+    reviews = relationship("Review", back_populates="borrower")
+
+    def __repr__(self):
+        return f"Borrower(id={self.id},first_name={self.first_name},last_name={self.last_name})"
+
+
+#######################################################
+class Review(Base):
+    __tablename__ = "reviews"
+
+    id = Column(Integer, primary_key=True)
+    star_rating = Column(Integer, nullable=False)
+    comment = Column(String(50), nullable=True)
+    book_id = Column(Integer, ForeignKey("books.id"))
+    borrower_id = Column(Integer, ForeignKey("borrowers.id"))
+
+    book = relationship("Book", back_populates="reviews")
+    borrower = relationship("Borrower", back_populates="reviews")
+
+    def __repr__(self):
+        return f"Review(id={self.id},star_rating={self.star_rating},comment={self.comment})"
---
+++ seed.py
@@ -0,0 +1,75 @@
+# seeds.py
+
+from faker import Faker
+import random
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Book, Author, Genre, Borrower, Review
+
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///books2.db")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.query(Review).delete()
+    session.query(Book).delete()
+    session.query(Author).delete()
+    session.query(Genre).delete()
+    session.query(Borrower).delete()
+
+    fake = Faker()
+
+    def create_fake_authors(num_authors):
+        for _ in range(num_authors):
+            name = fake.name()
+            author = Author(name=name)
+            session.add(author)
+
+    def create_fake_books(num_books):
+        for _ in range(num_books):
+            title = fake.sentence(nb_words=3)
+            description = fake.paragraph()
+            author = create_fake_authors(1)
+            genre = create_fake_genres(1)
+            book = Book(
+                title=title, description=description, author=author, genre=genre
+            )
+            session.add(book)
+
+    def create_fake_genres(num_genres):
+        for _ in range(num_genres):
+            name = fake.word()
+            genre = Genre(name=name)
+            session.add(genre)
+
+    def create_fake_borrowers(num_borrowers):
+        for _ in range(num_borrowers):
+            first_name = fake.first_name()
+            last_name = fake.last_name()
+            borrower = Borrower(first_name=first_name, last_name=last_name)
+            session.add(borrower)
+
+    def create_fake_reviews(num_reviews):
+        books = session.query(Book).all()
+        borrowers = session.query(Borrower).all()
+
+        for _ in range(num_reviews):
+            star_rating = random.randint(1, 5)
+            comment = fake.paragraph()
+            book = random.choice(books)
+            borrower = random.choice(borrowers)
+            review = Review(
+                star_rating=star_rating, comment=comment, book=book, borrower=borrower
+            )
+            session.add(review)
+
+    # Generate the fake data
+
+    create_fake_authors(7)
+    create_fake_books(25)
+    create_fake_genres(4)
+    create_fake_borrowers(10)
+    create_fake_reviews(40)
+
+    # Commit the changes to the database
+    session.commit()
